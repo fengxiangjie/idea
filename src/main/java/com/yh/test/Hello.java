e609e52a4fe1d0d65ae2863429ff2e0958ca47b2
skk <shaokuankuan@boyunmkt.com>
2019-03-02 23:53:23 +0800

    111

---
+++ src/main/java/com/yh/test/Hello.java
@@ -0,0 +1,20 @@
+package com.yh.test;
+
+public class Hello {
+    public static void main(String[] args) {
+        int a =1;
+        int b = a+2;
+
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        aa();
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+
+    public static void aa() {
+        System.out.println("aa");
+    }
+}
