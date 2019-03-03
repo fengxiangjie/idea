de2cd5fdaa7fb28e6809f716a09dcef18aa8a4dd
skk <shaokuankuan@boyunmkt.com>
2019-03-03 18:37:41 +0800

    sdsa

--- src/main/java/com/yh/test/Hello.java
+++ src/main/java/com/yh/test/Hello.java
@@ -2,14 +2,6 @@ package com.yh.test;
 
 public class Hello {
     public static void main(String[] args) {
-        int a =1;
-        int b = a+2;
-        System.out.println("dsa");
 
-
-    }
-
-    public static void aa() {
-        System.out.println("aa");
     }
 }
